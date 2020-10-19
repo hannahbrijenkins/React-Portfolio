edd2985b537d7a11f6cce67962a2cee3547405be
Hannah Jenkins <hannahbrittany@hotmail.com>
2020-10-19 00:39:48 -0400

    added nav bar

--- react-portfolio/src/App.js
+++ react-portfolio/src/App.js
@@ -2,10 +2,12 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import About from './components/About'
+import Header from './components/Header'
 
 function App() {
   return (
     <div>
+      <Header></Header>
       <main>
         <About></About>
       </main>
---
+++ react-portfolio/src/components/Header/index.js
@@ -0,0 +1,29 @@
+import React from 'react';
+
+function Header() {
+    return (
+        <header>
+            <h2>
+                <a href="/">Hannah Jenkins</a>
+            </h2>
+            <nav>
+                <ul>
+                    <li>
+                        <a href="#about">About Me</a>
+                    </li>
+                    <li>
+                        <a href="#portfolio">Portfolio</a>
+                    </li>
+                    <li>
+                        <a href="#contact">Contact</a>
+                    </li>
+                    <li>
+                        <a href="#resume">Resume</a>
+                    </li>
+                </ul>
+            </nav>
+        </header>
+    );
+}
+
+export default Header;
